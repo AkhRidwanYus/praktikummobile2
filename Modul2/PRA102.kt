4819d1602d6a52bd8e286302d72b57ae9ad78da8
AkhRidwanYus <101609315+AkhRidwanYus@users.noreply.github.com>
2022-03-18 15:20:43 +0800

    Create PRA102.kt

---
+++ Modul2/PRA102.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sken = Scanner(System. `in`)
+    print("Nilai x = ")
+    val x = sken.nextInt()
+
+    var hasil = (2 * (x * x)) + (5 * x) - 8
+    println("$hasil")
+}

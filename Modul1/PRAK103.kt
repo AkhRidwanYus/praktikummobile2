f4dcfac8692fd96d598eae1312304c83bab62f1b
AkhRidwanYus <101609315+AkhRidwanYus@users.noreply.github.com>
2022-03-22 18:56:19 +0800

    Update PRAK103.kt

--- Modul1/PRAK103.kt
+++ Modul1/PRAK103.kt
@@ -1,13 +1,9 @@
 import java.util.*
-
-fun main(args: Array<String>) {
-    val sken = Scanner(System. `in`)
-    print("Input Bilangan = ")
-    val bila = sken.nextInt()
+fun kelipatan(x: Int){
     var hitung = 0
     var ye: Int = 0
     for (i in 1..100){
-        ye = bila * i
+        ye = x * i
         if(((ye%2) == 0) or ((ye%3) == 0)){
             print("$ye ")
             hitung += 1
@@ -16,4 +12,10 @@ fun main(args: Array<String>) {
             break;
         }
     }
+}
+
+fun main(args: Array<String>) {
+    val sken = Scanner(System. `in`)
+    print("Input Bilangan = ")
+    kelipatan(sken.nextInt())
 }
